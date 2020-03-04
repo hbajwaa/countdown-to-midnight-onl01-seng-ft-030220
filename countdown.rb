a6daefb1035d41ff52e9d7e956ef9a970b9af55a
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-04 08:13:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,7 @@ def countdown
   
   while number > 0
     puts "#{number} SECOND(S)!"
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
   
@@ -17,6 +18,7 @@ def countdown_with_sleep
   while number > 0
     puts "#{number} SECOND(S)!"
     sleep 1
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
   
