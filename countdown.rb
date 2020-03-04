7f422c687af99e68e479e2126f09ca8392daa2a8
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-04 08:12:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+
 
 def countdown
+  number = 5
+  
+  while number > 0
+    puts "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+  
 end
+puts"------"
+
+def countdown_with_sleep
+  number = 5
+  
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+  
+end
+countdown
+countdown_with_sleep
